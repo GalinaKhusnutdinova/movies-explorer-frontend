41cf7a5f63ef17f8ae4f79b9d51366a3fa1f50f7
Galina Khusnutdinova <khusnutdinova.gal@yandex.ru>
2022-08-30 01:50:23 +0300

    feat

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./MoviesCard.css";
 
-export default function MoviesCard({film, children, images}) {
-
+export default function MoviesCard({ film, children, images }) {
   let filmUrl = `https://api.nomoreparties.co${images || ""}`;
 
   return (
@@ -11,7 +10,14 @@ export default function MoviesCard({film, children, images}) {
         <h3 className="card__name">{film.nameRU || film.nameEN}</h3>
         <p className="card__time">{`${film.duration} минут`}</p>
       </div>
-      <img className="card__img" src={filmUrl} alt="обложка фильма" />
+      <a
+        href={film.trailerLink}
+        target="_blank"
+        rel="noreferrer"
+        className="card__link"
+      >
+        <img className="card__img" src={filmUrl} alt="обложка фильма" />
+      </a>
       {children}
     </div>
   );
