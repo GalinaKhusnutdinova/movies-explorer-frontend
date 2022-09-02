b8a5119fdde3ccc9b4f21b1c043595720d95799f
Galina Khusnutdinova <khusnutdinova.gal@yandex.ru>
2022-09-02 13:44:50 +0300

    feat: добавление ссылок в readme

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,3 @@
-export const regexUrl =
-  /^[a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
+export const regexUrl = /^[a-z0-9-_.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
 
-export const BASE_URL =`${window.location.protocol}${
-		process.env.REACT_APP_API_URL || "//localhost:3000"
-	}`;
+export const BASE_URL =`${window.location.protocol}${process.env.REACT_APP_API_URL || "//localhost:3000"}`;
